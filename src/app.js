69b0f3baf87ae3eb57d7da9756fc4dc58431dc64
Geethanjali-04 <geethag2021@gmail.com>
2024-12-11 11:17:58 +0530

    get,post,delete,put,patch user (http methods)

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req,res)=>{
-   console.log("server listening");
-   res.send("test server listening");
+// match the get api
+app.get("/user", (req,res)=>{
+    res.send({"Name": "Jaggu", "age": 22});
 })
 
-app.use("/", (req,res)=>{
-    res.send("home page");
+// match the http post method
+app.post("/user", (req,res)=>{
+    res.send("user saved successfully");
 })
 
-app.use("/dev", (req,res)=>{
-    res.send("dev server listening");
+// match the http put method
+app.put("/user", (req,res)=>{
+    res.send("Data updated successfully");
 })
+
+// match the http patch method
+app.patch("/user", (req,res)=>{
+    res.send("column updated successfully");
+})
+
+// match the http delete method
+app.delete("/user", (req, res)=>{
+    res.send("user deleted successfully");
+})
+
+// default which matches all the http methods
+app.use("/user", (req, res)=>{
+    res.send({"name":"geetha", age: 22});
+})
+
 app.listen(3000, ()=>{
     console.log("SERVER RUNNING !!!");
 })
