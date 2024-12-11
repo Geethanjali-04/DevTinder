b1a36a1724b43492bb9b4b6ce60c2d5b0d3edf25
Geethanjali-04 <geethag2021@gmail.com>
2024-12-11 11:40:26 +0530

    playing wid routes using query params and req params

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 
 // match the get api
+// handling routes using query params
 app.get("/user", (req,res)=>{
+    console.log(req.query);
     res.send({"Name": "Jaggu", "age": 22});
 })
 
+// handling routes using req params 
+app.get("/user/:id/:password", (req,res)=>{
+    console.log(req.params);
+    res.send({"Name": "devs", "age": 22});
+})
 // match the http post method
 app.post("/user", (req,res)=>{
     res.send("user saved successfully");
