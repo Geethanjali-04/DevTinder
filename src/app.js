86b9faf67012a443bbdc52261c77a0ffb5f059db
Geethanjali-04 <geethag2021@gmail.com>
2024-12-17 23:58:39 +0530

    insert users dynamically in sign-up api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const {userAuth} = require("./middlewares/auth.js");
 const connectDb = require("./config/database.js");
 
+
+// connecting mongoDb
 connectDb().
 then(()=> {
     console.log("DB connected successfully!!");
@@ -10,3 +12,21 @@ then(()=> {
     console.log("SERVER RUNNING !!!");
 })})
 .catch((err)=> console.log("error connecting to db"));
+
+// require user model
+const User = require("./models/user.js");
+
+// middleware to accept json object in js object in req.body
+app.use(express.json());
+
+// sigup api
+app.post("/sign-up", async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("user saved successfully");
+    }
+    catch(err) {
+        res.send("user not saved successfully "+ err);
+    }
+})
