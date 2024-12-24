241092afdcada36657c41d3f7ec63c0341b8179c
Geethanjali-04 <geethag2021@gmail.com>
2024-12-24 11:12:16 +0530

    removed db config

--- src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const connectionUrl = "mongodb+srv://geethanjaligovindaraju:D3tksEdhP0VRhiFC@node-dev.us6o3.mongodb.net/devTinder"
-
-const connectDb = async () => await mongoose.connect("mongodb+srv://geethanjaligovindaraju:D3tksEdhP0VRhiFC@node-dev.us6o3.mongodb.net/devTinder")
-
-module.exports = connectDb;
